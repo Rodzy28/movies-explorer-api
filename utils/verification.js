d251242a4576e8f90bd3bf4cfef36528489a9f15
Sergei Pradko <rodzy28@yandex.ru>
2023-09-03 20:25:01 +0300

    Исправленеи ошибок

--- utils/verification.js
+++ utils/verification.js
@@ -1,6 +1,6 @@
 const { Joi, celebrate } = require('celebrate');
 
-const reg = /^https?:\/\/[a-z0-9а-я-._~:\\/?#[\]%@!$&'()*+,;=]*\.[a-z0-9а-я-._~:\\/?#[\]@%!$&'()*+,;=]*$/i;
+const reg = /https?:\/\/[a-z0-9а-я-._~:\\/?#[\]%@!$&'()*+,;=]*\.[a-z0-9а-я-._~:\\/?#[\]@%!$&'()*+,;=]*/i;
 
 const createUserVerification = celebrate({
   body: Joi.object().keys({
@@ -28,13 +28,13 @@ const createMovieVerification = celebrate({
   body: Joi.object().keys({
     country: Joi.string().required(),
     director: Joi.string().required(),
-    duration: Joi.string().required(),
+    duration: Joi.number().required(),
     year: Joi.string().required(),
     description: Joi.string().required(),
     image: Joi.string().required().regex(reg),
     trailerLink: Joi.string().required().regex(reg),
     thumbnail: Joi.string().required().regex(reg),
-    movieId: Joi.string().required(),
+    movieId: Joi.number().required(),
     nameRU: Joi.string().required(),
     nameEN: Joi.string().required(),
   }),
